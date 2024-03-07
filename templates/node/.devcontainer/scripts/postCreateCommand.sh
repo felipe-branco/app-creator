bd9e00cdff5817980199db2222bc86c909dc5b98
Felipe Branco <fbadiniz@gmail.com>
2024-03-07 12:16:01 -0300

    Update node devcontainer post creation script

--- templates/node/.devcontainer/scripts/postCreateCommand.sh
+++ templates/node/.devcontainer/scripts/postCreateCommand.sh
@@ -1,6 +1,10 @@
-#!/bin/sh -v
+#!/bin/zsh
 
-npm init -y
-npm i -g pnpm
-pnpm i -w -D typescript jest ts-jest prettier eslint eslint-config-prettier esbuild esbuild-node-externals
-pnpm i -w -D @types/jest @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser
+[ -e package.json ] && APP_ALREADY_CREATED=true || APP_ALREADY_CREATED=false
+
+if [ "$APP_ALREADY_CREATED" = false ]; then
+  npm init -y
+  npm i -g pnpm
+  pnpm i -w -D typescript jest ts-jest prettier eslint eslint-config-prettier esbuild esbuild-node-externals
+  pnpm i -w -D @types/jest @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser
+fi
